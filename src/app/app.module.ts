89fbbf6c540c2ce50b8f921204e175ac0be7ffa8
info@alkimiadigital.it <richross1999@gmail.com>
2022-01-29 14:20:12 +0100

    gitignore update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,20 @@ import { environment } from 'src/environments/environment';
 import { provideFirebaseApp , initializeApp} from '@angular/fire/app';
 //Firestore import
 import { getFirestore, provideFirestore} from '@angular/fire/firestore';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     provideFirebaseApp(()=>initializeApp(environment.firebase)),
-    provideFirestore(()=>getFirestore())
+    provideFirestore(()=>getFirestore()),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
